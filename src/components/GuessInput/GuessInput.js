8fffc033305b2c2f07600abdb2185a3f1a274739
shermanhui <11592023+shermanhui@users.noreply.github.com>
2023-10-08 11:01:59 -0700

    fix: pattern matching
    it should actually just be one set of [] instead of two, this
    was breaking the validation logic
    
    abstract out onChange function

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -9,17 +9,22 @@ function GuessInput() {
     setGuess("");
   };
 
+
+  const handleChange = (e) => {
+    setGuess(e.target.value.toUpperCase())
+  }
+
   return (
     <form className="guess-input-wrapper" onSubmit={onSubmitGuess}>
       <label htmlFor="guess-input">Enter guess:</label>
       <input
         id="guess-input"
         type="text"
-        onChange={(e) => setGuess(e.target.value.toUpperCase())}
+        onChange={handleChange}
         value={guess}
         minLength={5}
         maxLength={5}
-        pattern={"[a-z][A-Z]{5}"}
+        pattern={"[a-zA-Z]{5}"}
         title="5 letter word"
       ></input>
     </form>
